548ab263b9819a730fc7b9005fa65f060b760f83
sjucyweg <103584336+sjucyweg@users.noreply.github.com>
2022-04-13 23:00:03 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,10 +465,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '舍不得吃？';
+        if (cps <= 8) return 'up味道怎么样';
+        if (cps <= 10)  return 'up味道不错吧';
+        if (cps <= 15) return '不许再吃up了！！';
         return '人？';
     }
 
